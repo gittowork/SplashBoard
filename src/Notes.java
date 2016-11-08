e9dbc340438ee6f4a6f073e7aaebc3d02e2f6280
cphan_v2cgym4 <cphan_v2cgym4@10.15.181.57>
2016-11-07 16:56:02 -0800

    Notes class w/ added variables and constructor.

--- src/Notes.java
+++ src/Notes.java
@@ -1,4 +1,20 @@
 
 public class Notes {
-
+	protected String title;
+	protected String note;
+	protected int month;
+	protected int day;
+	protected int year;
+	protected String back;
+	
+	public Notes (String t, String n, int m, int d, int y, String b) {
+		title = t;
+		note = n;
+		month = m;
+		day = d;
+		year = y;
+		back = b;
+	}
+	
+	
 }
