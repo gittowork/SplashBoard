0db47fcd85870b17b9a73ad293a9a3ef34f76d4e
Nico <Nico@10.15.143.65>
2016-11-07 16:46:17 -0800

    Variables all completed

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,4 +1,21 @@
 
 public class Calendar {
-//Created new Calendar Class
+
+protected int month;
+protected int day;
+protected int year; 
+protected int startmin;
+protected int starthour;
+protected int endmin;
+protected int endhour;
+protected String back;
+protected String title;
+protected String description;
+protected Boolean allday;
+protected Boolean meridian;
+
+
+
+	
+	
 }
