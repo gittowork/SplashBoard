34867cd08edddb9ed0aaa2a02e14358cc36092e0
Nico <Nico@10.15.143.65>
2016-11-07 18:37:14 -0800

    Base Calendar Class

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,3 +1,6 @@
+import javax.swing.JFrame;
+
+import acm.graphics.*;
 
 public class Calendar {
 
@@ -13,9 +16,43 @@ protected String title;
 protected String description;
 protected Boolean allday;
 protected Boolean meridian;
+protected int PROGRAM_HEIGHT = 600;
+protected int PROGRAM_WIDTH = 800;
+JFrame frame = new JFrame("FrameDemo");
 
 
+public void yearScreen(GImage o){
 
 	
 	
+}
+
+
+
+public int getData(){
+	
+	return 0;
+}
+
+public void monthScreen(GImage o){
+	
+}
+
+public void removeEvent(){
+	
+}
+
+public void addDayScreen(){
+	
+}
+
+public void addImages(){
+	
+}
+
+public void addButtons(){
+	
+}
+
+
 }
