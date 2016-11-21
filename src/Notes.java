8e04cc6fdf4cad15c0de4acdb125773529d789f6
Cynthia <c_phan4@u.pacific.edu>
2016-11-20 22:03:17 -0800

    Started note body.

--- src/Notes.java
+++ src/Notes.java
@@ -1,12 +1,18 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
 
 import sun.util.resources.cldr.aa.CalendarData_aa_ER;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 
@@ -15,9 +21,12 @@ public class Notes {
 	protected int day;
 	protected int year;
 	protected String back;
-	protected JFrame frm = new JFrame();
+	private JFrame frm = new JFrame();
 	protected static int PROGRAM_HEIGHT = 1280; // Standard largest preferred size of screen.
 	protected static int PROGRAM_WIDTH = 1024;
+	private JPanel p1;
+	private JPanel p2;
+	private JButton bb;
 	
 	public static void main(String[] args) {
 		// This is equivalent to "run".
@@ -74,11 +83,26 @@ public class Notes {
 	
 	public void addNoteScreen() {
 	frm.setPreferredSize(new Dimension(PROGRAM_HEIGHT, PROGRAM_WIDTH)); //Screen dimension.
+	
+	// Note Title:
+	p1 = new JPanel();
 	JTextField title = new JTextField("Note Title");
 	title.setEditable(true);
 	title.setLocation(683, 384);
-	title.setFont(new Font("Serif",Font.PLAIN,60)); // Sets font style and size of font. 
-	frm.getContentPane().add(title, BorderLayout.CENTER);
+	title.setFont(new Font("Calibri",Font.PLAIN,60)); // Sets font style and size of font. 
+	p1.
+	p1.add(title, BorderLayout.CENTER);
+	frm.getContentPane().add(p1, BorderLayout.NORTH);
+	
+	// Note Body:
+	JTextField body = new JTextField("1111111");
+	body.setEditable(true);
+	frm.add(body, BorderLayout.CENTER);
+	JTextField body2 = new JTextField("222222");
+	body2.setEditable(true);
+	body.add(body2, BorderLayout.NORTH);
+	
+	frm.setForeground(Color.WHITE);
 	frm.pack(); // Packs all content onto screen; this is necessary in order for your content to appear when you run. 
 	frm.setVisible(true);
 	}
