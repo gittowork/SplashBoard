fa92e14094285687099efd9282ff13aceeeb37a5
Cynthia <c_phan4@u.pacific.edu>
2016-11-21 00:12:56 -0800

    Added more comments.

--- src/Notes.java
+++ src/Notes.java
@@ -3,14 +3,18 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.color.ColorSpace;
 
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.border.Border;
 
+import com.sun.corba.se.impl.orb.ParserTable.TestORBInitializer1;
 import com.sun.org.apache.bcel.internal.generic.NEW;
 
 import javafx.geometry.Point3D;
 import javafx.scene.shape.Shape3D;
+import sun.net.www.content.audio.x_aiff;
 import sun.util.resources.cldr.aa.CalendarData_aa_ER;
 
 import javax.swing.BorderFactory;
@@ -30,7 +34,6 @@ public class Notes {
 	protected static int PROGRAM_WIDTH = 1024;
 	private JPanel p1;
 	private JPanel p2;
-	private JPanel p3;
 	private JButton save;
 	private JButton backButton;
 	
@@ -86,8 +89,8 @@ public class Notes {
 	
 	// Note Title:
 	p1 = new JPanel();
-	p1.setSize(900, 500);
-	JTextArea title = new JTextArea(1, 2);
+	JTextArea title = new JTextArea(1, 15);
+	title.setLineWrap(true); // Wraps text to new line if it exceeds spec. width.
 	title.setEditable(true);
 	title.setFont(new Font("Calibri",Font.PLAIN,60)); // Sets font style and size of font. 
 	JButton b1 = new JButton();
@@ -99,28 +102,28 @@ public class Notes {
 	frm.getContentPane().add(p1, BorderLayout.NORTH);
 	
 	// Note Body:
-	p2 = new JPanel();
 	JTextArea body = new JTextArea(100, 100);
+	body.setLineWrap(true); // Wraps text to new line if it exceeds spec. width.
 	body.setEditable(true);
 	body.setFont(new Font("Calibri", Font.PLAIN, 28));
 	JButton b2 = new JButton();
 	TextFieldHint h2 = new TextFieldHint(body, "Start typing..."); // Sets text field hint.
 	b2.setContentAreaFilled(false); // Make button transparent.
 	b2.setBorderPainted(false);
-	frm.getContentPane().add(body, BorderLayout.CENTER);
+	frm.getContentPane().add(body, BorderLayout.CENTER); // Adds body to the center of frame.
 	
 	// Save Button:
-	p3 = new JPanel();
+	p2 = new JPanel();
 	save = new JButton("Save");
-	p3.add(save);
-	p1.add(p3, BorderLayout.EAST);
+	p2.add(save);
+	p1.add(p2, BorderLayout.EAST); // Adds save button to East side of the North panel.
 	
 	// Back Button:
 	
 	
 	frm.pack(); // Packs all content onto screen; this is necessary in order for your content to appear when you run. 
 	frm.setVisible(true);
-	frm.getContentPane().setForeground(Color.WHITE);
+	frm.getContentPane().setForeground(Color.WHITE); // Still doesn't work?? What the heck.........
 	b1.grabFocus();
 	b2.grabFocus();
 	}
