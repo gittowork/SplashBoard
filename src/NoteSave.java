e92992df1abea263145dc75fc9ec341caa519e45
Cynthia <c_phan4@u.pacific.edu>
2016-11-29 16:27:10 -0800

    Took out date fields.

--- src/NoteSave.java
+++ src/NoteSave.java
@@ -1,28 +1,10 @@
 public class NoteSave {
-		private int month;
-		private int day;
-		private int year;
 		private String t;
 		private String b;
 		
-		public NoteSave(int m, int d, int y, String title, String body){
-			month = m;
-			day = d;
-			year = y;
+		public NoteSave(String title, String body){
 			t = title;
 			b = body;
-		}
-		
-		public void setMonth(int m){
-			month = m;
-		}
-		
-		public void setDay(int d){
-			day = d;
-		}
-		
-		public void setYear(int y){
-			year = y;
 		}
 		
 		public void setTitle(String title) {
@@ -31,18 +13,6 @@ public class NoteSave {
 		
 		public void setBody(String body) {
 			b = body;
-		}
-
-		public int getMonth(){
-			return month;
-		}
-		
-		public int getDay(){
-			return day;
-		}
-		
-		public int getYear(){
-			return year;
 		}
 		
 		public String getTitle() {
